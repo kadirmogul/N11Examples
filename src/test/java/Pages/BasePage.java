260f0a5aefd3310294a4b37090904b861a69e07c
Abdul Kadir Moğul <158027198+kadirmogul@users.noreply.github.com>
2024-12-12 17:12:48 +0300

    Update BasePage.java

--- src/test/java/Pages/BasePage.java
+++ src/test/java/Pages/BasePage.java
@@ -1,4 +1,29 @@
 package Pages;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+
+import org.openqa.selenium.*;
+import org.openqa.selenium.support.FindBy;
+import org.testng.asserts.SoftAssert;
+
+
 public class BasePage {
+    protected WebDriver driver;
+
+    public BasePage(WebDriver driver){
+        this.driver=driver;
+    }
+    public void getBrowserTitle(){
+        driver.getTitle();
+    }
+    public void getPageURL(){
+        driver.getCurrentUrl();
+    }
+    public void getUrl(String urlAdress){
+        driver.get(urlAdress);
+
+    }
+
 }
